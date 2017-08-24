e377ec042a050592601276ffd79f09304741b5b5
Silverbeard00 <silverbeard@protonmail.com>
2017-08-24 15:09:41 -0400

    Add argument to script

--- find-network.sh
+++ find-network.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap 192.168.22.0/24 -p 85 --open  | grep -B 1 "Host is up" | sed -e '/^Host.*$/d' -e 's/^[^0-9]*//' -e '/^$/d'
+nmap $1 -p 85 --open  | grep -B 1 "Host is up" | sed -e '/^Host.*$/d' -e 's/^[^0-9]*//' -e '/^$/d'
