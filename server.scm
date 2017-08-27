21650ebcba95d525b9dd4dfce582af0f749041a2
Jack Lucas <silverbeard@protonmail.com>
2017-08-26 21:50:58 -0400

    recording with segmentation using ffmpeg

--- server.scm
+++ server.scm
@@ -1,6 +1,8 @@
 (require-extension tcp-server posix)
 (declare (uses tcp posix srfi-18))
 
+
+
 (define (generate-ip-port str)
   (define-values (ip ip2 ip3)
     (process
@@ -12,9 +14,9 @@
       '()
       (cons (read-line ipp) (generate-ip-lst ipp))))
 
+(define *camera-ips* (generate-ip-port "10.0.0.0/24"))
+
 (define (start-server)
-  (define *camera-ips* (generate-ip-port "10.0.0.0/24"))
-  (define *fake-camera-ips* '("10.0.0.23" "10.0.0.3" "10.32.3.4"))
   ((make-tcp-server
     (tcp-listen 6509)
     (lambda () (map (lambda (x)
@@ -24,8 +26,25 @@
 
 (thread-start! (make-thread (lambda () (start-server))))
 
-(thread-start!
- (make-thread
-  (lambda ()
-    (process "ffmpeg -i rtsp://admin:123456@10.0.0.48/profile1 -rtsp_transport tcp -r 10 -vcodec copy -an -t 900 output.mkv -y
-"))))
+(define (record-stream ip)
+  (thread-start!
+   (make-thread
+    (lambda ()
+      (process (string-append "ffmpeg -i rtsp://admin:123456@"
+                              ip
+                              "/profile1 -rtsp_transport tcp -r 10 -vcodec copy -y -segment_time"
+                              *segment-duration*
+                              "-f segment -an camera-1-%03d.mkv"))))))
+
+;;;Perhaps a symbol list for disabling/enabling ffmpeg options?
+;;;Or a text file?
+
+;;;Length of video files
+(define *segment-duration* "30")
+
+(define (record-all)
+  (map record-stream *camera-ips*))
+
+(record-all)
+
+
